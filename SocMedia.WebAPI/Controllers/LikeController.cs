53d8a37f8c4c80cd7c6487cdcd83aa1c2b959bac
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-09-01 08:53:25 -0400

    Added Like Service and Controller

--- SocMedia.WebAPI/Controllers/LikeController.cs
+++ SocMedia.WebAPI/Controllers/LikeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SocMedia.Models;
+using SocMedia.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,8 +10,27 @@ using System.Web.Http;
 
 namespace SocMedia.WebAPI.Controllers
 {
+    [Authorize]
     public class LikeController : ApiController
     {
-        //public IHttpActionResult PostLikebyPostId(int id)        
+        private LikeService CreateLikeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var LikeService = new LikeService(userId);
+            return LikeService;
+        }
+        [HttpPost]
+        public IHttpActionResult PostLikebyPostId(LikeCreate like)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateLikeService();
+
+            if (!service.CreateLike(like))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
